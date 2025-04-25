5387156db51eb9f069df0f981d73442d91b40965
Nicolas <nkrieg@student.ethz.ch>
2025-04-25 14:46:51 -0700

    Current state adaptive (not efficient)

--- config.py
+++ config.py
@@ -13,16 +13,16 @@ TIME_STEP = 1.0        # Time step in seconds
 # Scaling factors for the simulation
 Sbar_scaling = 1.1
 
-hack_nodes2 = ["s701a", "s701b", "s701c"]
 hack_nodes = ["s701a", "s701b"]
+hack_nodes2 = ["s701a", "s701b", "s701c"]
 #hack_nodes = node_names  # Uncomment to attack all nodes
 bp_override = [0.994, 0.995, 0.995, 0.996, 0.997] # Example breakpoint override
 bp_701a = [0.974351955, 1.004351955, 1.004351955, 1.034351955, 1.064351955]
 
 hacks_list = [
-    #[100, 200, 0.4, bp_override, hack_nodes],
+    [100, 200, 0.4, bp_override, hack_nodes],
     #[100, 200, 1.0, bp_override, 1.0],
-    [150, 250, 0.5, None, hack_nodes2],
+    [150, 250, 0.5, bp_override, hack_nodes2],
     #[100, 200, 0.5, bp_override, ["s701a"]],
-    #[100, 180, 0.5, bp_701a, ["s701a"]],
+    #[100, 180, 1.0, bp_701a, ["s701a"]],
 ]
